63bf0c3d591179d724b2e28bb93981293617b884
michodev <mikemaggi71@gmail.com>
2026-01-31 23:44:21 -0500

    este si funciono

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
-const API_URL = 'https://iptv-api-two.onrender.com/admin';
-// const API_URL = 'http://localhost:3001/admin';
+//const API_URL = 'https://iptv-api-two.onrender.com/admin';
+ const API_URL = 'http://localhost:3001/admin';
 let ADMIN_KEY = sessionStorage.getItem('adminKey') || '';
 // Init
 document.addEventListener('DOMContentLoaded', () => {
